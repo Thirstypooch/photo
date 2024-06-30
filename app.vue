9bbe9221fc17de32db1fa30dd337430a18c0481f
juan cocchi <juan.cocchi@cristalino.pe>
2024-06-30 18:49:57 -0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Camera Page</h1>
+    <CameraCapture />
+  </div>
+</template>
---
+++ pages/camera.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Camera Page</h1>
+    <CameraCapture />
+  </div>
+</template>
+
+<script>
+import CameraCapture from '~/components/CameraCapture.vue';
+
+export default {
+  components: {
+    CameraCapture,
+  },
+};
+</script>
