895bae985ec65b991adf6103b14f99334637f8d1
Thirstypooch <juancocchi6@gmail.com>
2025-05-16 12:52:21 -0500

    updated project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   pages: false,
+  nitro: {
+    compatibilityDate: '2025-05-16'
+  }
 })
